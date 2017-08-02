65ddfcf2ffd11e3c7f5e0bba603eb7d7ab97c222
Daniel <danielr_soyyo@hotmail.com>
2017-08-02 18:39:55 +0100

    CORS article resources

---
+++ CORS/main.js
@@ -0,0 +1,43 @@
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+
+        // Check if the XMLHttpRequest object has a "withCredentials" property.
+        // "withCredentials" only exists on XMLHTTPRequest2 objects.
+        xhr.open(method, url, true);
+
+
+        xhr.onload = function() {
+            var responseText = JSON.parse(xhr.responseText);
+            responseText.forEach((show) => {
+                    console.log(show);
+                })
+                // process the response.
+        };
+
+        xhr.onerror = function() {
+            console.log('There was an error!');
+        };
+
+    } else if (typeof XDomainRequest != "undefined") {
+
+        // Otherwise, check if XDomainRequest.
+        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+
+    } else {
+
+        // Otherwise, CORS is not supported by the browser.
+        xhr = null;
+
+    }
+    return xhr;
+}
+
+var xhr = createCORSRequest('GET', 'http://api.tvmaze.com/shows', true);
+if (!xhr) {
+    throw new Error('CORS not supported');
+}
+
+xhr.send();
